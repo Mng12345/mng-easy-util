f4b10785ca3c33614f420f2614d14d2d23cb26d5
zhangming <1570977353@qq.com>
2020-11-23 22:02:30 +0800

    add index for easy import

---
+++ dist/index.d.ts
@@ -0,0 +1,14 @@
+import * as algorithm from './algorithm';
+import * as clone from './clone';
+import * as events from './events';
+import * as file from './file';
+import * as funcUtil from './func-util';
+import * as hashmap from './hashmap';
+import * as hashset from './hashset';
+import * as json from './json';
+import * as math from './math';
+import * as minMax from './min-max';
+import * as object from './object';
+import * as pointer from './pointer';
+import * as stream from './stream';
+export { algorithm, clone, events, file, funcUtil, hashmap, hashset, json, math, minMax, object, pointer, stream };
---
+++ lib/algorithm/index.ts
@@ -0,0 +1,9 @@
+import * as curveFit from './curve-fit'
+import * as ga from './ga';
+import * as mergeAxis from './merge-axis'
+
+export {
+    curveFit,
+    ga,
+    mergeAxis
+}
